d48aec0e132488d3f97eec84176b1206f6fbf053
kimura-khamali <kimurakhama@gmail.com>
2024-02-28 22:12:42 +0300

    add files

--- pip.py
+++ pip.py
@@ -1,25 +1,20 @@
-print("select option")
-print("1. ADD")
-print("2. SUBTRACTION")
-print("3. MULTIPLICATION")
-print("4. DIVISION")
+#multiplication
+m=20
+p=10
+result=m*p
+
+#Division
+m=20
+p=5
+results=m/p
 
 
+#Addation
+m=10
+p=4
+results=m+p
 
-operation = input ()
-if operation == "1":
- num1 = input ("enter first number:")
- num2 = input ("enter second number:")
- print("the sum is: " + str(int(num1) + int(num2)))
-elif operation == "2":
-    num1 = input ("enter first number:")
-    num2 = input ("enter second number:")
-    print("the subtraction is: " + str(int(num1)-int(num2)))
-elif operation == "3":
-    num1 = input ("enter first number:") 
-    num2 = input ("enter second number:") 
-    print("the multiplication is: " + str(int(num1)*int(num2)))
-elif operation == "4":
-    num1 = input ("enter first number:")
-    num2 = input ("enter second number:")  
-    print("the division is:" + str(int(num1)/int(num2)))
+#Subtraction
+m=14
+p=10
+results=m-p
